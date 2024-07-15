a70935eb26a627228114449ee2a1dccd44218b29
Armando Luja <aluja@amazon.com>
2024-07-15 14:06:47 -0700

    chore: fix test

--- packages/reference-auth-construct/src/construct.test.ts
+++ packages/reference-auth-construct/src/construct.test.ts
@@ -7,8 +7,11 @@ void describe('AmplifyConstruct', () => {
     const app = new App();
     const stack = new Stack(app);
     new AmplifyReferenceAuth(stack, 'test', {
-      authRoleArn: 'authRoleArn',
-      unauthRoleArn: 'unauthRoleArn',
+      authRoleArn:
+        'arn:aws:cognito-idp:us-east-1:000000000000:userpool/us-east-1_IDSAMPLE1',
+      unauthRoleArn:
+        // eslint-disable-next-line spellcheck/spell-checker
+        'arn:aws:cognito-identity:us-east-1:000000000000:identitypool/us-east-1:00000000-abcd-efgh-ijkl-000000000000',
       identityPoolId: 'identityPoolId',
       userPoolClientId: 'userPoolClientId',
       userPoolId: 'userPoolId',
